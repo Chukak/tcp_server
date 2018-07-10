211eefbfc1608d35f4e8833f79338e4103f762cf
Chukak <chukak12@gmail.com>
2018-07-10 20:19:00 +0300

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <cstdlib>
+#include "include/server.h"
+#include <iostream>
 
 using namespace std;
 
@@ -6,7 +8,19 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    try {
+        if (argc != 3) {
+            return 1;
+        }
+        
+        server s(argv[1], argv[2]);
+        
+        s.run();
+        
+    } catch (std::exception& e) {
+        cout << e.what() << "\n";
+        return 1;
+    }
     return 0;
 }
 
