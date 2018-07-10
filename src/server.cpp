810d04b7b23d01d4f71e1bc1c4ec4174513811b7
Chukak <chukak12@gmail.com>
2018-07-10 20:16:51 +0300

    definition for the `server` class

---
+++ src/server.cpp
@@ -0,0 +1,62 @@
+#include "../include/server.h"
+#include <boost/lexical_cast.hpp>
+
+
+server::server(const std::string& addr, const std::string& port) :
+    io(),
+    acceptor_(io),
+    signals(io)
+{
+    signals.add(SIGINT);
+    signals.add(SIGTERM);
+#ifdef SIGQUIT
+    signals.add(SIGQUIT);
+#endif
+    
+    do_await_stop();
+    
+    tcp::endpoint endpoint;
+    try {
+        endpoint = tcp::endpoint(boost::asio::ip::address::from_string(addr), 
+                    boost::lexical_cast<int>(port)); 
+    } catch (boost::bad_lexical_cast& e) {
+        tcp::resolver resolver(io);
+        endpoint = *resolver.resolve(addr, port).begin(); 
+    }
+    acceptor_.open(endpoint.protocol());
+    acceptor_.set_option(acceptor::reuse_address(true));
+    acceptor_.bind(endpoint);
+    acceptor_.listen();
+    
+    do_accept();
+}
+
+void server::run() 
+{
+    io.run();
+}
+
+void server::do_accept()
+{
+    acceptor_.async_accept(
+        [this](const sys_ec& ec, sock s)
+        {
+            if (acceptor_.is_open()) { 
+                if (!ec) {
+                    connection_manager_.start(
+                        boost::make_shared<connection>(s, connection_manager_)); 
+                }
+                do_accept(); 
+            }
+        });
+}
+
+void server::do_await_stop()
+{
+    signals.async_wait(
+        [this](const sys_ec& ec, size_t)
+        {
+            acceptor_.close();
+            connection_manager_.stop_all();
+        });
+}
